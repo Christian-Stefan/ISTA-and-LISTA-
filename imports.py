aaca91c6014a0586784218d83953c630bd100e62
Cristian <c.gheorghiu1@student.tue.nl>
2026-06-13 00:08:37 +0200

    First commit of the essential imports

---
+++ imports.py
@@ -0,0 +1,20 @@
+import ctypes as ct
+import random as rdm
+import torch.nn as nn
+import torch
+import pandas as pd
+import matplotlib.pyplot as PLT
+from torchinfo import summary
+from CodeForStudents4.MNIST_dataloader import create_dataloaders
+import argparse
+from torch.optim import (
+    SGD,RAdam, Adam
+)
+from torch.nn import (
+    BCELoss, MSELoss
+)
+from torchvision.transforms import v2
+import os
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import tqdm
