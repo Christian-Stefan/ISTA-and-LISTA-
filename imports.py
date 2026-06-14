a299e71b2339394c2737da90520048f65b7e5bf0
Gheorghiu <c.gheorghiu1@student.tue.nl>
2026-06-14 15:54:33 +0200

    <Major>: Completion of Exercise 4.3

--- imports.py
+++ imports.py
@@ -6,6 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as PLT
 from torchinfo import summary
 from CodeForStudents4.MNIST_dataloader import create_dataloaders
+from CodeForStudents4.Fast_MRI_dataloader import create_dataloaders as create_dataloaders_MRI
 import argparse
 from torch.optim import (
     SGD,RAdam, Adam
